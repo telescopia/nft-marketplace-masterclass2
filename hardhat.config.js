a2eb67e2fb8c509bf38bf27242095cbd1af4f504
Nick B <telescopiabiz@gmail.com>
2022-03-20 18:29:40 +0300

    updated configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,28 @@
+require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");;
+require("@nomiclabs/hardhat-etherscan");
+
+const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY, ETHERSCAN_API_KEY, NETWORK } = process.env;
 
 module.exports = {
-  defaultNetwork: "ropsten",
+  defaultNetwork: NETWORK,
   networks: {
     hardhat: {
     },
     ropsten: {
-      url: "https://eth-ropsten.alchemyapi.io/v2/SF5vUrTZCdaE-145B6TZSxmj_TZOhS93",
-      accounts: ["516e006872ebe4f404d1df36a7d63883fa79ae50f071d58daf5e1f7061c13318", "f185edba521795923c3d85f086fb99c68c23e73bff118af9bb7400366b849423"]
-    }
-  },
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
+   },
   solidity: "0.8.12",
   paths: {
     artifacts: "./src/backend/artifacts",
     sources: "./src/backend/contracts",
     cache: "./src/backend/cache",
     tests: "./src/backend/test"
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
